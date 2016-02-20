2902202ca77396959842276fba2dabdec67045ed
perlancar (@pc-home) <perlancar@gmail.com>
2016-02-21 00:20:47 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/Import/NEILB.pm
@@ -0,0 +1,20 @@
+package Acme::CPANLists::Import::NEILB;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CPANLists imported from http://neilb.org/reviews/
+
+=head1 DESCRIPTION
+
+This distribution contains module lists that are imported from Neil Bowers'
+(CPANID:NEILB) module review articles located at L<http://neilb.org/reviews/>.
+This distribution is created to improve the "mentions cloud".
+
+
+=head1 SEE ALSO
+
+L<Acme::CPANLists>
+
+L<App::lcpan> and L<lcpan>, particularly the B<related-mods> subcommand.
